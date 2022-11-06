442b862d9d174ce97cff936bd82bb8ccdd98736c
Zu_rin <s165810t@gmail.com>
2022-11-06 12:00:10 +0900

    update visual copy btn

--- js/create_copy_btn.js
+++ js/create_copy_btn.js
@@ -1,26 +1,26 @@
 const addCopyBtn = (elm) => {
-    let btn = document.createElement("button");
-    btn.classList.add("copy-btn");
-    btn.textContent = "copy"
+    let btn = document.createElement("div");
+    btn.insertAdjacentHTML("beforeend", '<i class="fa fa-fw fa-light fa-copy"></i>');
+    btn.classList.add("btn", "btn-sm", "pull-right");
     btn.onclick = () => {
         const tagstr = elm.querySelector("div > div.code").textContent;
         navigator.clipboard.writeText(tagstr)
             .then(() => {
-                console.log("COPY");
-                console.log(tagstr);
+                console.log("COPY", tagstr);
             })
             .catch(err => {
                 console.log("ERROR", err);
             });
     }
-    elm.appendChild(btn);
+    elm.parentNode.before(btn);
 }
 
-window.onload = (e) => {
-    const elms = document.querySelectorAll("pre > code")
-    console.log(elms);
-    elms.forEach(elm => {
-        console.log(elm.textContent);
-        addCopyBtn(elm);
-    });
-};
+window.addEventListener("load", (e) => {
+    // mermaid・graphvizの描画前に"pre>code"にマッチする要素が出現してしまうため描画待機 (TODO: 改善)
+    setTimeout(() => {
+        const elms = document.querySelectorAll("pre > code");
+        elms.forEach(elm => {
+            addCopyBtn(elm);
+        }) 
+    }, 2000)
+}, false);
